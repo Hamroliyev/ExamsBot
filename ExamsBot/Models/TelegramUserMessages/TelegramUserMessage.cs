0d3346fc9a49cabdca5c1d9df7f3e53a8f12bdcf
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-09-02 13:36:03 +0500

    DATA: Create TelegramUserMessage Model

---
+++ ExamsBot/Models/TelegramUserMessages/TelegramUserMessage.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using ExamsBot.Models.TelegramUsers;
+using Telegram.Bot.Types;
+
+namespace ExamsBot.Models.TelegramUserMessages
+{
+    public class TelegramUserMessage
+    {
+        public TelegramUser TelegramUser { get; set; }
+        public Message Message { get; set; }
+    }
+}
