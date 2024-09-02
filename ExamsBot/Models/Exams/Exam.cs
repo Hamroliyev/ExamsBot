2ed1599c0f0f35f28a2c254a149470167a8a90e5
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-09-02 13:35:40 +0500

    DATA: Create Exam Model

---
+++ ExamsBot/Models/Exams/Exam.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using ExamsBot.Models.TelegramUsers;
+using System;
+
+namespace ExamsBot.Models.Exams
+{
+    public class Exam
+    {
+        public Guid Id { get; set; }
+        public int CountOfTests { get; set; }
+        public DateTime CreatedDate { get; set; } 
+        public Guid TelegramUserId { get; set; }
+        public TelegramUser TelegramUser { get; set; }
+    }
+}
