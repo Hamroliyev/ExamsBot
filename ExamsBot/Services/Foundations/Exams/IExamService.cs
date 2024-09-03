48eeaeab0f8c253ed6dfe562eb3390264477e3b5
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-09-03 11:57:49 +0500

    FOUNDATIONS: Create Foundations Services.

---
+++ ExamsBot/Services/Foundations/Exams/IExamService.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using ExamsBot.Models.Exams;
+
+namespace ExamsBot.Services.Foundations.Exams
+{
+    public interface IExamService
+    {
+        ValueTask<Exam> AddExamAsync(Exam exam);
+        IQueryable<Exam> RetriveAllExams();
+        ValueTask<Exam> RetrieveExamByIdAsync(Guid examId);
+        ValueTask<Exam> ModifyExamAsync(Exam exam);
+        ValueTask<Exam> RemoveExamAsync(Exam exam);
+    }
+}
