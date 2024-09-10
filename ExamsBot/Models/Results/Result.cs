ca1e5676354eb5c8d50e033be1c4f33df0231df4
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-09-10 17:21:19 +0500

    DATA: Initialize Result Model

---
+++ ExamsBot/Models/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using ExamsBot.Models.Exams;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using ExamsBot.Models.TelegramUsers;
+
+namespace ExamsBot.Models.Results
+{
+    public class Result
+    {
+        public int ResultId { get; set; }
+        public int StudentId { get; set; }
+        public Guid ExamId { get; set; }
+        public decimal Score { get; set; }
+        public string Grade { get; set; }
+        public TelegramUser TelegramUser { get; set; }
+        public Exam Exam { get; set; }
+    }
+}
