bcdba12abad30de349edd90f062518a08e100b50
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-09-11 20:20:10 +0500

    PROCESSINGS: Initialize Exam Processing Service

---
+++ ExamsBot/Services/Processings/Exams/IExamProcessingService.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using ExamsBot.Models.Exams;
+using System.Threading.Tasks;
+
+namespace ExamsBot.Services.Processings.Exams
+{
+    public interface IExamProcessingService
+    {
+        ValueTask<Exam> ModifyTelegramUserAsync(Exam exam);
+        ValueTask<Exam> UpsertTelegramUserProcessingService(Exam exam);
+    }
+}
