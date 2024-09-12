089fb3a6dbf12c51755584bc35b8d296c6813aa9
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-09-12 16:19:45 +0500

    PROCESSINGS: TelegramUserProcessingService Is Initialized

---
+++ ExamsBot/Services/Processings/Telegrams/TelegramUserProcessingService.cs
@@ -0,0 +1,36 @@
+﻿    // Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using ExamsBot.Models.TelegramUsers;
+using ExamsBot.Services.Foundations.TelegramUsers;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ExamsBot.Services.Processings.Telegrams
+{
+    public class TelegramUserProcessingService : ITelegramUserProcessingService
+    {
+        private readonly ITelegramUserService telegramUserService;
+
+        public TelegramUserProcessingService(ITelegramUserService telegramUserService)
+        {
+            this.telegramUserService = telegramUserService;
+        }
+
+        public async ValueTask<TelegramUser> ModifyTelegramUserAsync(TelegramUser telegramUser) =>
+            await this.telegramUserService.ModifyTelegramUserAsync(telegramUser);
+
+        public async ValueTask<TelegramUser> UpsertTelegramUserProcessingService(
+            TelegramUser telegramUser)
+        {
+            TelegramUser maybeTelegramUser =
+                await this.telegramUserService.RetriveAllTelegramUsers()
+                    .FirstOrDefaultAsync(user => user.TelegramId == telegramUser.TelegramId);
+
+            return maybeTelegramUser is null
+                ? await this.telegramUserService.AddTelegramUserAsync(telegramUser)
+                : maybeTelegramUser;
+        }
+    }
+}
