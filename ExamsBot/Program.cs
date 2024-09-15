685e0f621d20cf4c67aeb7ebf9be99c351048512
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-09-15 17:48:26 +0500

    CODE RUB: Init Program.cs

--- ExamsBot/Program.cs
+++ ExamsBot/Program.cs
@@ -1,25 +1,55 @@
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using ExamsBot.Brokers.Storages;
+using ExamsBot.Brokers.Telegrams;
+using ExamsBot.Services.Foundations.Exams;
+using ExamsBot.Services.Foundations.Telegrams;
+using ExamsBot.Services.Foundations.TelegramUsers;
+using ExamsBot.Services.Orchestrations.Telegrams;
+using ExamsBot.Services.Processings.Exams;
+using ExamsBot.Services.Processings.Telegrams;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+AddBrokers(builder);
+AddFoundationServices(builder);
+AddProcessingServices(builder);
+AddOrchestrationServices(builder);
 
 var app = builder.Build();
+RegisterEventListeners(app);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+static void AddBrokers(WebApplicationBuilder builder)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddTransient<IStorageBroker, StorageBroker>();
+    builder.Services.AddSingleton<ITelegramBroker, TelegramBroker>();
 }
 
-app.UseHttpsRedirection();
+static void AddFoundationServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddTransient<ITelegramService, TelegramService>();
+    builder.Services.AddTransient<ITelegramUserService, TelegramUserService>();
+    builder.Services.AddTransient<IExamService, ExamService>();
+}
 
-app.UseAuthorization();
+static void AddProcessingServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddTransient<ITelegramUserProcessingService, TelegramUserProcessingService>();
+    builder.Services.AddTransient<IExamProcessingService, ExamProcessingService>(); 
+}
+static void AddOrchestrationServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddTransient<ITelegramUserOrchestrationService, TelegramUserOrchestrationService>();
+}
 
-app.MapControllers();
+static void RegisterEventListeners(IApplicationBuilder app)
+{
+    app.ApplicationServices.GetRequiredService<ITelegramUserOrchestrationService>()
+                .ListenTelegramUserMessage();
+}
 
 app.Run();
