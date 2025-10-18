b364caaaec1dd51cc2923ef241d88109c5f8871b
Kroos <hamroliyevahmadjon1909@gmail.com>
2025-10-18 20:42:00 +0500

    DATA: Create Stduent Model

---
+++ ExamsBot/Models/IAuditable.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+
+namespace ExamsBot.Models
+{
+    public interface IAuditable
+    {
+        DateTimeOffset CreatedDate { get; set; }
+        DateTimeOffset UpdatedDate { get; set; }
+        Guid CreatedBy { get; set; }
+        Guid UpdatedBy { get; set; }
+    }
+}
---
+++ ExamsBot/Models/Students/Gender.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+namespace ExamsBot.Models.Students
+{
+    public enum Gender
+    {
+        Female,
+        Male,
+        Other,
+        Unknown
+    }
+}
