609ddb55f218c17dbf80d87db7450774e371c652
Kroos <hamroliyevahmadjon1909@gmail.com>
2025-12-24 16:58:16 +0500

    brokers(users): define IUserManagementBroker interface
    - Add CRUD method signatures for User entity
    - Include password-specific insertion for Identity
    - Utilize ValueTask and IQueryable for performance and deferred execution

---
+++ ExamsBot/Brokers/UserManagement/IUserManagementBroker.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamsBot.Models.Users;
+
+namespace ExamsBot.Brokers.UserManagement
+{
+    public interface IUserManagementBroker
+    {
+        ValueTask<User> InsertUserAsync(User user, string password);
+        IQueryable<User> SelectAllUsers();
+        ValueTask<User> SelectUserByIdAsync(Guid userId);
+        ValueTask<User> UpdateUserAsync(User user);
+        ValueTask<User> DeleteUserAsync(User user);
+    }
+}
