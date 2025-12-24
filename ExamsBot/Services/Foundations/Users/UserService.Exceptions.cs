f08dad8dd954984c56e0c7f4a872cd2b40e27df5
Kroos <hamroliyevahmadjon1909@gmail.com>
2025-12-24 17:08:50 +0500

    services(users): implement exception handling and try-catch logic
    - Centralize error handling using delegate patterns
    - Map infrastructure exceptions (SQL, EF, DB) to domain exceptions
    - Implement logging orchestration for Validation, Dependency, and Service errors
    - Ensure critical dependency failures are logged with high priority

---
+++ ExamsBot/Services/Foundations/Users/UserService.Exceptions.cs
@@ -0,0 +1,131 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
+using ExamsBot.Models.Users;
+using ExamsBot.Models.Users.Exceptions;
+using Xeptions;
+
+namespace ExamsBot.Services.Foundations.Users
+{
+    public partial class UserService
+    {
+        private delegate ValueTask<User> ReturningUserFunction();
+        private delegate IQueryable<User> ReturningQueryableUserFunction();
+
+        private async ValueTask<User> TryCatch(ReturningUserFunction returningUserFunction)
+        {
+            try
+            {
+                return await returningUserFunction();
+            }
+            catch (NullUserException nullUserException)
+            {
+                throw CreateAndLogValidationException(nullUserException);
+            }
+            catch (InvalidUserException invalidUserException)
+            {
+                throw CreateAndLogValidationException(invalidUserException);
+            }
+            catch (NotFoundUserException nullUserException)
+            {
+                throw CreateAndLogValidationException(nullUserException);
+            }
+            catch (DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistsUserException =
+                    new AlreadyExistsUserException(duplicateKeyException);
+
+                throw CreateAndLogValidationException(alreadyExistsUserException);
+            }
+            catch (SqlException sqlException)
+            {
+                var failedUserStorageException =
+                    new FailedUserStorageException(sqlException);
+
+                throw CreateAndLogCriticalDependencyException(failedUserStorageException);
+            }
+            catch (DbUpdateConcurrencyException dbUpdateConcurrencyException)
+            {
+                var lockedUserException =
+                    new LockedUserException(dbUpdateConcurrencyException);
+
+                throw CreateAndLogDependencyException(lockedUserException);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                var failedUserStorageException =
+                    new FailedUserStorageException(dbUpdateException);
+
+                throw CreateAndLogDependencyException(failedUserStorageException);
+            }
+            catch (Exception exception)
+            {
+                var failedUserServiceException =
+                    new FailedUserServiceException(exception);
+
+                throw CreateAndLogServiceException(failedUserServiceException);
+            }
+        }
+
+        private IQueryable<User> TryCatch(ReturningQueryableUserFunction returningQueryableUserFunction)
+        {
+            try
+            {
+                return returningQueryableUserFunction();
+            }
+            catch (SqlException sqlException)
+            {
+                var failedUserStorageException =
+                    new FailedUserStorageException(sqlException);
+
+                throw CreateAndLogCriticalDependencyException(failedUserStorageException);
+            }
+            catch (Exception exception)
+            {
+                var failedUserServiceException =
+                    new FailedUserServiceException(exception);
+
+                throw CreateAndLogServiceException(failedUserServiceException);
+            }
+        }
+
+        private Exception CreateAndLogValidationException(Exception exception)
+        {
+            var userValidationException = new UserValidationException(exception);
+            this.loggingBroker.LogError(userValidationException);
+
+            return userValidationException;
+        }
+
+        private UserDependencyException CreateAndLogDependencyException(Exception exception)
+        {
+            var userDependencyException = new UserDependencyException(exception);
+            this.loggingBroker.LogError(userDependencyException);
+
+            return userDependencyException;
+        }
+
+        private UserDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var userDependencyException = new UserDependencyException(exception);
+            this.loggingBroker.LogCritical(userDependencyException);
+
+            return userDependencyException;
+        }
+
+        private UserServiceException CreateAndLogServiceException(Exception exception)
+        {
+            var userServiceException = new UserServiceException(exception);
+            this.loggingBroker.LogError(userServiceException);
+
+            return userServiceException;
+        }
+    }
+}
