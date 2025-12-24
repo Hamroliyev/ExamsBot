e894c9bd7061661f960c64963a4de3a77bd18e27
Kroos <hamroliyevahmadjon1909@gmail.com>
2025-12-24 16:21:24 +0500

    feat(models): add Gender enum to project models
    Introduces the Gender enumeration with Female, Male, Other, and
    Unknown types to support user/student profiling.

---
+++ ExamsBot/Models/Gender.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+namespace ExamsBot.Models
+{
+    public enum Gender
+    {
+        Female,
+        Male,
+        Other,
+        Unknown
+    }
+}
